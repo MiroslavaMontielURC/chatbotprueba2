71b102db78f34943c87896dd4d5ef8d878de8237
MiroslavaMontielURC <mirosmontielirc@gmail.com>
2025-01-07 20:51:36 -0600

    Update chat.js

--- chat.js
+++ chat.js
@@ -1,1 +1,8 @@
+import { createChat } from 'https://cdn.jsdelivr.net/npm/@n8n/chat/dist/chat.bundle.es.js';
+
+window.onload = () => {
+    createChat({
+        webhookUrl: 'https://n8n-tutorial-7tma.onrender.com/webhook/4a70aa85-f5e6-4858-acf7-c62db86c6b7e/chat',
+    });
+};
 
